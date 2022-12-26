4dce7c5e7caaf469504754ea6b89366ee5e07415
Leshamir <anastasy34@gmail.com>
2022-12-26 20:04:56 +0200

    Add project files.

---
+++ main.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include "Fox.h"
+#include "Rabbit.h"
+#include "Grass.h"
+#include "AbsenceOfLife.h"
+using namespace std;
+
+int main() {
+	Info* info = nullptr;
+
+	int choose = 0;
+	cout << "1. Fox\n2. Rabbit\n3. Grass\n-> ";
+	cin >> choose;
+	cin.ignore();
+
+	switch (choose) {
+	case 1:
+		info = new Fox();
+		break;
+
+	case 2:
+		info = new Rabbit();
+		break;
+
+	case 3:
+		info = new Grass();
+		break;
+	}
+	info->Input();
+	info->Print();
+	
+	AbsenceOfLife obj;
+	obj.Input();
+	obj.Compare();
+
+	
+
+}
+
